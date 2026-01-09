8b73dc6675572fe070b42b07683e29abac367e92
SEKOzoz <46aisports46@gmail.com>
2026-01-09 13:29:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function toggleMenu() {
     sidebar.style.left = "0px";
   }
 }
+const password = "inutero"; // BURAYI DEĞİŞTİR
+
+window.onload = function () {
+  const input = prompt("Giriş kelimesi:");
+
+  if (input === password) {
+    document.getElementById("protectedContent").style.display = "block";
+  } else {
+    document.body.innerHTML = "<h2 style='color:white;text-align:center;margin-top:40vh;'>Erişim reddedildi</h2>";
+  }
+};
