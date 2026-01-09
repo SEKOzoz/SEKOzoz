5492b2fc0ca2814608a70fe85a381a26277b6027
SEKOzoz <46aisports46@gmail.com>
2026-01-09 13:51:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
   document.getElementById("sidebar").classList.toggle("active");
 }
+function checkPassword() {
+  const correctPassword = "nirvana"; // şifre BURASI
+  const input = document.getElementById("passwordInput").value;
+
+  if (input === correctPassword) {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("protectedContent").style.display = "block";
+  } else {
+    document.getElementById("error").innerText = "Yanlış şifre";
+  }
+}
