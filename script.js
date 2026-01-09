ef7071c4e746b0dc8683cd33c697c60c2c6b4191
SEKOzoz <46aisports46@gmail.com>
2026-01-09 14:05:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
-const PASSWORD = "nirvana2";
-const ACCESS_MS = 60 * 1000; // 1 dakika
+alert("JS YÜKLENDİ");
 
 function toggleMenu() {
   document.getElementById("sidebar").classList.toggle("active");
 }
-
 function checkPassword() {
-  const input = document.getElementById("passwordInput").value.trim();
+  const correctPassword = "nirvana"; // şifre BURASI
+  const input = document.getElementById("passwordInput").value;
 
-  if (input === PASSWORD) {
-    const expireTime = Date.now() + ACCESS_MS;
-    localStorage.setItem("expireTime", expireTime);
-    grantAccess();
+  if (input === correctPassword) {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("protectedContent").style.display = "block";
   } else {
     document.getElementById("error").innerText = "Yanlış şifre";
   }
 }
-
-function grantAccess() {
-  document.getElementById("loginBox").style.display = "none";
-  document.getElementById("protectedContent").style.display = "block";
-}
-
-/* SAYFA AÇILINCA KONTROL */
-function checkAccess() {
-  const expireTime = localStorage.getItem("expireTime");
-  if (!expireTime) return;
-
-  if (Date.now() < expireTime) {
-    grantAccess();
-
-    // ⏳ süre dolunca OTOMATİK AT
-    setTimeout(() => {
-      localStorage.removeItem("expireTime");
-      location.reload();
-    }, expireTime - Date.now());
-
-  } else {
-    localStorage.removeItem("expireTime");
-  }
-}
-
-checkAccess();
